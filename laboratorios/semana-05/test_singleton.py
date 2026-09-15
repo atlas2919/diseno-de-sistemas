5d30b9c716f8cec924268a12ed7c74c0873e5038
Alex Yong <kronus2919@MacBook-Pro-3.local>
2026-09-15 14:08:21 -0500

    Add labs semana-05 singleton and test singleton

--- laboratorios/semana-05/test_singleton.py
+++ laboratorios/semana-05/test_singleton.py
@@ -1,16 +1,18 @@
 from singleton import GestorDeConfiguracion, reserva_permitida
 
+def setup_function():
+    GestorDeConfiguracion._objeto = None
+
 # Las pruebas tienen que ir en funciones
 def test_rechaza_reserva():
     config = GestorDeConfiguracion.obtener_objeto()
     config.modo_mantenimiento = True
-    
+
     ## assert sirve para hacer preguntas
     assert reserva_permitida(config) is False
-    
+
 def test_reserva_aceptada():
     config = GestorDeConfiguracion.obtener_objeto()
-    config.modo_mantenimiento = False
     assert reserva_permitida(config) is True
     
 ## Falla porque el primer test modifica el objeto compartido y no lo devuelve al estado original
