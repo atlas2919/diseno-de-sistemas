cca2ef083558fd187ff93e2d39118c373bfd0b44
Alex Yong <kronus2919@MacBook-Pro-3.local>
2026-09-17 13:56:07 -0500

    semana-05

---
+++ laboratorios/semana-05/reservas.py
@@ -0,0 +1,98 @@
+class Estudiante:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        
+class EquipoOficial:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        
+class ReservaRegular:
+    def __init__(self, cancha, fecha, hora_inicio, hora_fin, solicitante):
+        self.cancha = cancha
+        self.fecha = fecha
+        self.hora_inicio = hora_inicio
+        self.hora_fin = hora_fin
+        self.solicitante = solicitante
+        
+    def confirmar(self):
+        return "Reserva confirmada para " + self.solicitante.nombre
+        
+class ReservaPrioridad:
+    def __init__(self, cancha, fecha, hora_inicio, hora_fin, solicitante):
+            self.cancha = cancha
+            self.fecha = fecha
+            self.hora_inicio = hora_inicio
+            self.hora_fin = hora_fin
+            self.solicitante = solicitante
+            
+    def confirmar(self):
+        return "Reserva con prioridad confirmada para " + self.solicitante.nombre
+
+class CreadorDeReserva():
+    def crear_reserva(self, cancha, fecha, hora_inicio, hora_fin, solicitante):
+        raise NotImplementedError
+    
+class CreadorDeReservaRegular(CreadorDeReserva):
+    def crear_reserva(self, cancha, fecha, hora_inicio, hora_fin, solicitante):
+        return ReservaRegular(
+            cancha,
+            fecha,
+            hora_inicio,
+            hora_fin,
+            solicitante
+        )
+        
+class CreadorDeReservaPrioritaria(CreadorDeReserva):
+    def crear_reserva(self, cancha, fecha, hora_inicio, hora_fin, solicitante):
+        return ReservaPrioridad(
+            cancha,
+            fecha,
+            hora_inicio,
+            hora_fin,
+            solicitante
+        )
+        
+class FabricaDeReserva():
+    @staticmethod
+    def elegir_creador(solicitante):
+        if isinstance(solicitante, Estudiante):
+            return CreadorDeReservaRegular()
+        elif isinstance(solicitante, EquipoOficial):
+            return CreadorDeReservaPrioritaria()
+     
+def reservar_desde_web(cancha, fecha, hora_inicio, hora_fin, solicitante):
+    
+    creador = FabricaDeReserva.elegir_creador(solicitante)
+    reserva = creador.crear_reserva(
+        cancha, 
+        fecha,
+        hora_inicio,
+        hora_fin,
+        solicitante
+    )  
+    
+    return reserva
+
+def main():
+    reserva_1 = reservar_desde_web(
+        'Cancha de futbol',
+        '2026-09-17',
+        '18:00',
+        '20:00',
+        Estudiante('Alex')
+    )
+    
+    print(reserva_1.confirmar())
+    
+    reserva_2 = reservar_desde_web(
+            'Cancha de futbol',
+            '2026-09-17',
+            '18:00',
+            '20:00',
+            EquipoOficial('Alex capitan')
+        )
+    
+    print(reserva_2.confirmar())
+
+if __name__ == "__main__":
+    main()
